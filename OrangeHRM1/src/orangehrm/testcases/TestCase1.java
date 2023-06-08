7bda651caaf414b918699dcd8e26f43f8d635423
Prakash <dhanaprakash1998@gmail.com>
2023-06-08 19:25:07 +0530

    First Commit:TestCase1 Added

---
+++ OrangeHRM1/src/orangehrm/testcases/TestCase1.java
@@ -0,0 +1,11 @@
+package orangehrm.testcases;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is TestCase1");
+	}
+
+}
